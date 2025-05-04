9a97df2bcf3ee09548741c3dec40f236025dc3c7
Matias <3020772@tud.hs-mannheim.de>
2025-05-04 11:42:08 +0200

    PDFbox test errfolgreich

--- src/main/java/de/hs_mannheim/informatik/mvn/Main.java
+++ src/main/java/de/hs_mannheim/informatik/mvn/Main.java
@@ -1,10 +1,24 @@
 package de.hs_mannheim.informatik.mvn;
 
+import java.io.File;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		
+		String pdfFilePath = "src/main/resources/wc.pdf";
+		File pdfFile = new File(pdfFilePath);
+		try (PDDocument document = Loader.loadPDF(pdfFile)) {
+		PDFTextStripper pdfStripper = new PDFTextStripper();
+		String text = pdfStripper.getText(document);
+		System.out.println(text);
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
 		
 	}
 	
